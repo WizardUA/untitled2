30d0d0f399d1ebe0c2aa7b7a549c3e40d6a91ae5
WizardUA <boychuk-o@yandex.ua>
2015-06-05 16:22:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("HelloWorld");
+
+    }
+
+    public void Meth(int a){
+        System.out.printf("");
+    }
+
+}
